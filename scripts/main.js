f7018b20646e7be84c431964c79e137938897b35
pedro <pedro.gch01@gmail.com>
2022-07-18 23:02:36 -0300

    cambios varios

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 let menutoggle = document.querySelector('.toggle');
 let menu = document.querySelector('.menu');
+$(".sub-menu-info").hide();
 menutoggle.onclick =  function(){
   menutoggle.classList.toggle('active');
   menu.classList.toggle('mostrar');
   
 }
+let opMenuInfo = document.querySelector('.op-info');
+opMenuInfo.onclick= function(){
+  $(".sub-menu-info").toggle();
+
+}
+
